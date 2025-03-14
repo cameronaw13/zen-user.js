13a2f7ff6fed2f46c9fe1aac49e7408f88315bdb
cameronaw13 <cameronawichman@gmail.com>
2025-03-14 03:11:51 -0700

    user-overrides changes, userChrome big tab scrollbar

--- user-overrides.js
+++ user-overrides.js
@@ -1,9 +1,9 @@
 //-- Custom user.js Overrides --//
 
 /* DNS over HTTPS */
-//user_pref("network.trr.mode", 3); // Always use custom DoH
-user_pref("network.trr.mode", 5); // Never use DoH
-//user_pref("network.trr.uri", "https://sky.rethinkdns.com/1:-BMgAAAFAMAABABAIAAAKABA"); 
+//user_pref("network.trr.mode", 3); // Always use custom DoH // 0710
+user_pref("network.trr.mode", 5); // Never use DoH // 0710
+//user_pref("network.trr.uri", "https://sky.rethinkdns.com/1:-BMgAAAFAMAABABAIAAAKABA");  // 0712
 
 /* Firefox Homepage */
 //user_pref("browser.startup.page", 1); // Show 'homepage' on startup
@@ -16,13 +16,12 @@ user_pref("network.trr.mode", 5); // Never use DoH
 //user_pref("browser.toolbars.bookmarks.visibility", "always");
 
 /* Credentials */
-user_pref("extensions.formautofill.addresses.enabled", false);
-user_pref("extensions.formautofill.creditCards.enabled", false);
-user_pref("extensions.formautofill.heuristics.enabled", false);
-user_pref("signon.rememberSignons", false);
+user_pref("extensions.formautofill.addresses.enabled", false); // 5017
+user_pref("extensions.formautofill.creditCards.enabled", false); // 5017
+user_pref("extensions.formautofill.heuristics.enabled", false); 
+user_pref("signon.rememberSignons", false); // 5003
 
 /* URL Bar */
-//user_pref("keyword.enabled", true); // Url bar searching
 user_pref("browser.urlbar.suggest.history", false); // 5010
 user_pref("browser.urlbar.suggest.bookmark", false); // 5010
 user_pref("browser.urlbar.suggest.openpage", false); // 5010
@@ -30,13 +29,8 @@ user_pref("browser.urlbar.suggest.openpage", false); // 5010
 user_pref("browser.urlbar.suggest.engines", false); // 0815
 user_pref("browser.urlbar.trimURLs", false);
 
-/* Fingerprinting */ // Prefer usability over anonyminity, Will compensate w/ CanvasBlocker's canvas & audio randomizing
-user_pref("privacy.resistFingerprinting", false); // 4506
-user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504
-user_pref("webgl.disabled", false); // 4520
+/* WebGL */
 user_pref("webgl.force-enabled", true);
-//user_pref("network.http.referer.XOriginPolicy", 0); // 5510
-//user_pref("network.http.referer.XOriginTrimmingPolicy", 0); // 1602
 
 /* Session Restore */
 user_pref("privacy.clearOnShutdown.history", false); // 2811
@@ -44,19 +38,13 @@ user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2
 user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2811
 
 /* Remove Popups */
-user_pref("dom.popup_allowed_events", "click dblclick mousedown pointerdown");
+user_pref("dom.popup_allowed_events", "click dblclick mousedown pointerdown"); // 5018
 
 /* Scrollbar */
 user_pref("widget.non-native-theme.scrollbar.size.override", 16);
 
 /* PiP */
 user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
-
-/* Pocket */
-//user_pref("extensions.pocket.enabled", false);
-
-/* Tab list */
-//user_pref("browser.tabs.tabmanager.enabled", false);
 
 /* Stupid linux middle click paste */
 user_pref("clipboard.autocopy", false);

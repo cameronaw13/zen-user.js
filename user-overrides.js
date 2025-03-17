4ae50559423ddb36dae883e7facd9bbd718ad855
cameronaw13 <cameronawichman@gmail.com>
2025-03-16 20:00:32 -0700

    user-overrides cleanup & disable touchpad gesture

--- user-overrides.js
+++ user-overrides.js
@@ -5,27 +5,17 @@
 user_pref("network.trr.mode", 5); // Never use DoH // 0710
 //user_pref("network.trr.uri", "https://sky.rethinkdns.com/1:-BMgAAAFAMAABABAIAAAKABA");  // 0712
 
-/* Firefox Homepage */
-//user_pref("browser.startup.page", 1); // Show 'homepage' on startup
-//user_pref("browser.startup.homepage", "about:home");
-//user_pref("browser.newtabpage.enabled", true);
-//user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-//user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
-
-/* Bookmarks */
-//user_pref("browser.toolbars.bookmarks.visibility", "always");
-
 /* Credentials */
 user_pref("extensions.formautofill.addresses.enabled", false); // 5017
 user_pref("extensions.formautofill.creditCards.enabled", false); // 5017
-user_pref("extensions.formautofill.heuristics.enabled", false); 
+user_pref("extensions.formautofill.heuristics.enabled", false);
 user_pref("signon.rememberSignons", false); // 5003
 
 /* URL Bar */
 user_pref("browser.urlbar.suggest.history", false); // 5010
 user_pref("browser.urlbar.suggest.bookmark", false); // 5010
 user_pref("browser.urlbar.suggest.openpage", false); // 5010
-//user_pref("browser.urlbar.suggest.topsites", false); // 5010
+//user_pref("browser.urlbar.suggest.topsites", false); // 5010 //disabled due to zen urlbar bug
 user_pref("browser.urlbar.suggest.engines", false); // 0815
 user_pref("browser.urlbar.trimURLs", false);
 
@@ -40,6 +30,8 @@ user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2
 /* Remove Popups */
 user_pref("dom.popup_allowed_events", "click dblclick mousedown pointerdown"); // 5018
 
+//-- Browser Preferences --//
+
 /* Scrollbar */
 user_pref("widget.non-native-theme.scrollbar.size.override", 16);
 
@@ -49,6 +41,10 @@ user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
 /* Stupid linux middle click paste */
 user_pref("clipboard.autocopy", false);
 user_pref("middlemouse.paste", false);
+
+/* Disable back & forward touchpad gestures */
+user_pref("browser.gesture.swipe.left", "");
+user_pref("browser.gesture.swipe.right", "");
 
 /* Zen Browser */
 user_pref("zen.urlbar.behavior", "normal");
